9e9c7397db503cb2582dd6483df87f2693964bb8
Kacper Sokołowski <kontakt@kaapa.pl>
2021-11-02 21:19:54 +0100

    Zadania ze zmiennych

---
+++ obiekty/solution.js
@@ -0,0 +1,11 @@
+let car1 = {
+    mark: 'Mercedes',
+    model: 'C',
+    productionYear: 2020
+};
+
+let car2 = {
+    mark: 'Tesla',
+    model: 'X',
+    productionYear: 2019
+};
---
+++ zmienne/solution.js
@@ -0,0 +1,5 @@
+let C = 299792458;
+let m = 10
+
+let E = m * C * C;
+console.log(E);
